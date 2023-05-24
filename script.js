27fc5feec7958aad66f90f008a48a8e46755213e
Shashank Deshpande <128305551+ShashankD7@users.noreply.github.com>
2023-05-24 16:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function displayBrowserInfo() {
+	const browserName = navigator.appName;
+    const version = navigator.appVersion;
+	const message = "You are using " + browserName + " version " + version;
+	const browserInfoElement = document.getElementById("browser-info");
+	browserInfoElement.textContent = message;
+    }
+displayBrowserInfo();
